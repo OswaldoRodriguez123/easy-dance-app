dba52ba2dd5399adbb9d33313b994d2292809e8a
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2016-07-23 18:23:28 -0400

    Añadida migracion que elimina campos innecesarios en la tabla de regalos

---
+++ database/migrations/2016_07_23_221724_alter_regalos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterRegalosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::table('regalos', function (Blueprint $table) {
+            $table->dropColumn('dirigido_a');
+            $table->dropColumn('de_parte_de');
+            $table->dropColumn('correo');
+
+        });        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('regalos', function (Blueprint $table) {
+            $table->string('dirigido_a')->after('descripcion');
+            $table->string('de_parte_de')->after('dirigido_a');
+            $table->string('correo')->after('de_parte_de');
+        });        //
+    }
+}
