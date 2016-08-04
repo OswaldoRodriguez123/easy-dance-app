eb4ca1632c778d35624611ebfe93126978f2a35a
perfectdancer <latinoeasydance@gmail.com>
2016-08-04 15:06:31 -0430

    Corrección de caracteres pagina de matenimiento

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>EasyDance - en Mantenimiento</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100,400,700" rel="stylesheet" type="text/css">
