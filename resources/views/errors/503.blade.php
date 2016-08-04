f7c713e75bfcee1211cdb3c9438308becba6510b
perfectdancer <latinoeasydance@gmail.com>
2016-08-04 14:59:49 -0430

    Pagina de mantenimiento

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>EasyDance - en Mantenimiento</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,400,700" rel="stylesheet" type="text/css">
 
         <style>
             html, body {
@@ -18,6 +18,18 @@
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
+            }
+
+            p{
+                font-size: 30px;
+                font-weight: 400;
+                padding-left: 30px;
+                padding-right: 30px;
+            }
+
+            h1 { 
+                font-size: 50px; 
+                font-weight: 400;
             }
 
             .container {
@@ -40,7 +52,14 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title"></div>
+                <article>
+                    <h1>&iexcl;Regresaremos pronto!</h1>
+                    <div>
+                        <p>Pedimos disculpas por los incovenientes causados pero estamos trabajando en cosas interesantes.&iexcl;Pronto estaremos en línea de nuevo!</p>
+                        <p>&mdash; El equipo de easydancelatino.com</p>
+                    </div>
+                </article>
             </div>
         </div>
     </body>
