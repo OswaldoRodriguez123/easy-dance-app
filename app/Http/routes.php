c9b406766dc0301b825930e95059b0501c7885fc
Oswaldo <bfsraptor@gmail.com>
2016-09-05 14:42:35 -0430

    prueba

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,15 +14,15 @@
 		return view('correo.ayuda');
 	});
 
+	Route::get('/confirmacion/{token}/{email}', [
+	    'uses' => 'RegistroController@confirmacion',
+	    'as'   => 'confirmacion'
+	]);
+
 
 Route::auth();
 
 Route::get('autologin/{token}', ['as' => 'autologin', 'uses' => '\Watson\Autologin\AutologinController@autologin']);
-
-Route::get('/confirmacion/{token}/{email}', [
-    'uses' => 'RegistroController@confirmacion',
-    'as'   => 'confirmacion'
-]);
 
 // FLUJO DE REGISTRO
 
