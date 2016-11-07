3b3df53f22a10861de050419abd130f41f6169d7
Oswaldo <bfsraptor@gmail.com>
2016-11-07 09:57:49 -0430

    Arreglando pequeños errores ortograficos

--- resources/views/asistencia/generar.blade.php
+++ resources/views/asistencia/generar.blade.php
@@ -47,8 +47,8 @@
                             <tbody>
 
                             @foreach ($alumnosacademia as $alumno)
-                                <?php $id = $producto['id']; ?>
-                                <tr id="row_{{$id}}" class="seleccion" >
+                                <?php $id = $alumno['id']; ?>
+                                <tr id="asistencia_alumno_row_{{$id}}" class="seleccion" data-imagen = "{{$alumno->imagen}}" data-id-participante = "{{$alumno->id}}" data-nombre-participante = "{{$alumno->nombre}} {{$alumno->apellido}}" data-identificacion-participante = "{{$alumno->identificacion}}" data-tipo-participante = "alumno" data-sexo = "{{$alumno->sexo}}">
                                     <td class="text-center previa">
                                     @if($alumno->imagen){
                                         <img class="lv-img-sm" src="{{url('/')}}/assets/uploads/usuario/'+array.imagen+'" alt="">
@@ -70,14 +70,6 @@
                         </table>
                          </div>
                         </div>
-                        <div class="card-body p-b-20">
-                            <div class="row">
-                              <div class="container">
-                                
-                              </div>
-                            </div>
-                        </div>
-                        
                         
                     </div>
                     
@@ -88,163 +80,57 @@
 
 @section('js') 
             
-        <script type="text/javascript">
-            
-            route_detalle="{{url('/')}}/configuracion/productos/detalle";
-            route_eliminar="{{url('/')}}/configuracion/productos/eliminar/";
+    <script type="text/javascript">
 
         $(document).ready(function(){
 
-        t=$('#tablelistar').DataTable({
-        processing: true,
-        serverSide: false,
-        pageLength: 25,  
-        order: [[0, 'asc']],
-        fnDrawCallback: function() {
-        if ("{{count($productos)}}" < 25) {
-              $('.dataTables_paginate').hide();
-              $('#tablelistar_length').hide();
-          }else{
-             $('.dataTables_paginate').show();
-          }
-        },
-        pageLength: 25,
-        language: {
-              searchPlaceholder: "Buscar"
-        },
-        fnRowCallback: function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-          $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4)', nRow).addClass( "text-center" );
-          $('td:eq(0),td:eq(1)', nRow).attr( "onclick","previa(this)" );
-        },
-        language: {
-                        processing:     "Procesando ...",
-                        search:         '<div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>',
-                        searchPlaceholder: "BUSCAR",
-                        lengthMenu:     "Mostrar _MENU_ Registros",
-                        info:           "Mostrando _START_ a _END_ de _TOTAL_ Registros",
-                        infoEmpty:      "Mostrando 0 a 0 de 0 Registros",
-                        infoFiltered:   "(filtrada de _MAX_ registros en total)",
-                        infoPostFix:    "",
-                        loadingRecords: "...",
-                        zeroRecords:    "No se encontraron registros coincidentes",
-                        emptyTable:     "No hay datos disponibles en la tabla",
-                        paginate: {
-                            first:      "Primero",
-                            previous:   "Anterior",
-                            next:       "Siguiente",
-                            last:       "Ultimo"
-                        },
-                        aria: {
-                            sortAscending:  ": habilitado para ordenar la columna en orden ascendente",
-                            sortDescending: ": habilitado para ordenar la columna en orden descendente"
+            t=$('#tablelistar').DataTable({
+            processing: true,
+            serverSide: false,
+            pageLength: 25,  
+            order: [[0, 'asc']],
+            fnDrawCallback: function() {
+            if ("{{count($alumnosacademia)}}" < 25) {
+                  $('.dataTables_paginate').hide();
+                  $('#tablelistar_length').hide();
+              }else{
+                 $('.dataTables_paginate').show();
+              }
+            },
+            pageLength: 25,
+            language: {
+                  searchPlaceholder: "Buscar"
+            },
+            fnRowCallback: function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+              $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4)', nRow).addClass( "text-center" );
+              $('td:eq(0),td:eq(1),td:eq(2)', nRow).attr( "onclick","buscar(this)" );
+            },
+            language: {
+                            processing:     "Procesando ...",
+                            search:         '<div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>',
+                            searchPlaceholder: "BUSCAR",
+                            lengthMenu:     "Mostrar _MENU_ Registros",
+                            info:           "Mostrando _START_ a _END_ de _TOTAL_ Registros",
+                            infoEmpty:      "Mostrando 0 a 0 de 0 Registros",
+                            infoFiltered:   "(filtrada de _MAX_ registros en total)",
+                            infoPostFix:    "",
+                            loadingRecords: "...",
+                            zeroRecords:    "No se encontraron registros coincidentes",
+                            emptyTable:     "No hay datos disponibles en la tabla",
+                            paginate: {
+                                first:      "Primero",
+                                previous:   "Anterior",
+                                next:       "Siguiente",
+                                last:       "Ultimo"
+                            },
+                            aria: {
+                                sortAscending:  ": habilitado para ordenar la columna en orden ascendente",
+                                sortDescending: ": habilitado para ordenar la columna en orden descendente"
+                            }
                         }
-                    }
-        });
-    
-
-            if($('.chosen')[0]) {
-                $('.chosen').chosen({
-                    width: '100%',
-                    allow_single_deselect: true
-                });
-            }
-            if ($('.date-time-picker')[0]) {
-               $('.date-time-picker').datetimepicker();
-            }
-
-            if ($('.date-picker')[0]) {
-                $('.date-picker').datetimepicker({
-                    format: 'DD/MM/YYYY'
-                });
-            }
-
-                //Basic Example
-                $("#data-table-basica").bootgrid({
-                    css: {
-                        icon: 'zmdi icon',
-                        iconColumns: 'zmdi-view-module',
-                        iconDown: 'zmdi-expand-more',
-                        iconRefresh: 'zmdi-refresh',
-                        iconUp: 'zmdi-expand-less'
-                    }
-                });
-            });
-
-        function previa(t){
-
-            var row = $(t).closest('tr').attr('id');
-            var id_producto = row.split('_');
-            var route =route_detalle+"/"+id_producto[1];
-            window.location=route;
-        }
-
-         $("i[name=operacion").click(function(){
-            var route =route_operacion+"/"+this.id;
-            window.location=route;
-         });
-
-         $("i[name=eliminar]").click(function(){
-                id = this.id;
-                element = this;
-                swal({   
-                    title: "Desea eliminar el producto?",   
-                    text: "Confirmar eliminación!",   
-                    type: "warning",   
-                    showCancelButton: true,   
-                    confirmButtonColor: "#DD6B55",   
-                    confirmButtonText: "Eliminar!",  
-                    cancelButtonText: "Cancelar",         
-                    closeOnConfirm: false 
-                }, function(isConfirm){   
-          if (isConfirm) {
-            console.log(this);
-            var nFrom = $(this).attr('data-from');
-            var nAlign = $(this).attr('data-align');
-            var nIcons = $(this).attr('data-icon');
-            var nType = 'success';
-            var nAnimIn = $(this).attr('data-animation-in');
-            var nAnimOut = $(this).attr('data-animation-out')
-            var nTitle="Ups! ";
-            var nMensaje="Ha ocurrido un error, intente nuevamente por favor";
-            // notify(nFrom, nAlign, nIcons, nType, nAnimIn, nAnimOut);
-            swal("Hecho!","Eliminado con éxito!","success");
-            
-            eliminar(id, element);
-                }
             });
         });
-      function eliminar(id, element){
-         var route = route_eliminar + id;
-         var token = "{{ csrf_token() }}";
-                
-                $.ajax({
-                    url: route,
-                        headers: {'X-CSRF-TOKEN': token},
-                        type: 'DELETE',
-                    dataType: 'json',
-                    data:id,
-                    success:function(respuesta){
 
-                    t.row( $(element).parents('tr') )
-                      .remove()
-                      .draw();
-
-                    },
-                    error:function(msj){
-                                $("#msj-danger").fadeIn(); 
-                                var text="";
-                                console.log(msj);
-                                var merror=msj.responseJSON;
-                                text += " <i class='glyphicon glyphicon-remove'></i> Por favor verifique los datos introducidos<br>";
-                                $("#msj-error").html(text);
-                                setTimeout(function(){
-                                         $("#msj-danger").fadeOut();
-                                        }, 3000);
-                                }
-                });
-      }
-
-
-        </script>
+    </script>
 
 @stop
