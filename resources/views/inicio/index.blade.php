0ea7ea4a1266738b2146d323524e0744e1489fc6
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-01-29 20:10:02 -0500

    Cambios del domingo

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -259,9 +259,20 @@
 <script>
 
     $(document).ready(function(){
-    
-    if(!"{{$academia->nombre}}")
 
+    function empty(str)
+    {
+        if (typeof str == 'undefined' || !str || str.length === 0 || str === "" || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+        
+    if(empty("{{$academia->nombre}}"))
     {
       setTimeout(function(){ 
 
