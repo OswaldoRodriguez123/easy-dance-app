0015561513aa38229f76a6d9dbdbc779f1ecd441
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-03-09 18:58:53 -0330

    Cambios del jueves

--- resources/views/configuracion/productos/principal.blade.php
+++ resources/views/configuracion/productos/principal.blade.php
@@ -117,7 +117,7 @@
         },
         fnRowCallback: function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
           $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4)', nRow).addClass( "text-center" );
-          $('td:eq(0),td:eq(1)', nRow).attr( "onclick","previa(this)" );
+          $('td:eq(0),td:eq(1),td:eq(2)', nRow).attr( "onclick","previa(this)" );
         },
         language: {
                         processing:     "Procesando ...",
