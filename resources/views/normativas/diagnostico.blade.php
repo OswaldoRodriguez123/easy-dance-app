b17e89944a7bbb38cb350bc50719f801a77cae19
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-03-15 20:39:24 -0330

    Cambios del martes

--- resources/views/normativas/diagnostico.blade.php
+++ resources/views/normativas/diagnostico.blade.php
@@ -31,28 +31,27 @@
                             <div class="text-center f-25 f-700">Normativas de diagnósticos de ingresos</div>
                             <hr>
                         </div>
-                        <div class="table-responsive row">
                         <div class="col-md-1"></div>
-                           <div class="col-md-10">
-<div class="text-justify">
+                          <div class="col-md-10">
+                            <div class="text-justify">
 
-<p>La organización ofrece un diagnóstico de ingreso completamente gratis, en los horarios establecidos por la organización, estos serán anunciado y agendados con anticipación para la preparación del cliente y el buen desarrollo de los procesos organizativos</p>
+                              <p>La organización ofrece un diagnóstico de ingreso completamente gratis en los horarios establecidos por la misma, estos serán anunciados y agendados con anticipación para la preparación del cliente y el buen desarrollo de los procesos organizativos:</p>
 
-                                      <p>1. Las fechas y horarios de los diagnósticos de ingresos serán ofrecidos por parte de la organización , en caso que el alumno no posea disponibilidad, el cliente podrá agendar en un horario de su preferencia, esta cita será considerada con un estatus de cita exclusiva, el cual, tiene un valor adicional económico  por una atención en horario de su conveniencia</p>
+                              <p>1. Las fechas y horarios de los diagnósticos de ingresos serán ofrecidos por parte de la organización, en caso que el alumno no posea disponibilidad de tiempo para el horario establecido, podrá agendar en un horario diferente y de su preferencia; esta cita será considerada con un estatus de cita exclusiva, el cual, tiene un valor económico adicional por la atención y procesos adicionales.</p>
 
-                                      <p>2. Todos los  participantes disfrutarán del  uso y grandes ventajas  de la aplicación tecnológica Easy Dance, sólo desde el momento de haber realizado su diagnóstico de ingreso.</p>
+                              <p>2. Todos los participantes disfrutarán del uso y grandes ventajas de la aplicación tecnológica Easy Dance, sólo desde el momento de haber realizado su diagnóstico de ingreso.</p>
 
-                                      <p>3. En caso que un participante llegue tarde a su cita de diagnóstico de ingreso, podrá acceder sólo si existiesen cupos disponibles para los siguientes horarios del día, así mismo deberá asumir una cuota de retraso valorada y anunciada por la organización.  </p>
+                              <p>3. En caso de que un participante llegue tarde a su cita de diagnóstico de ingreso, podrá acceder sólo en caso de existir cupos disponibles para los siguientes horarios del día, así mismo deberá asumir una cuota de retraso valorada y anunciada por la organización.</p>
 
-                                      <p>4. Después que el  participante haya agendado su cita de diagnóstico de ingreso a los horarios que establece la organización , y él (el cliente )  no pueda asistir por razones propias o ajena a su voluntad, el participante perderá el derecho de recibir dicha valoración de manera gratuita, la cita podrá ser agendada una vez más por el cliente en mutuo acuerdo con la organización, cambiando esta (la cita ) a estatus de <b>Cita exclusiva</b>  </p>
+                              <p>4. Después que el participante haya agendado su cita de diagnóstico de ingreso a los horarios que establece la organización , y este (el cliente) no pueda asistir por razones propias o ajenas a su voluntad, perderá el derecho de recibir dicha valoración de manera gratuita, la cita podrá ser agendada una vez más por el cliente en mutuo acuerdo con la organización, cambiandola (la cita) a estatus de <b>Cita exclusiva</b>  </p>
 
-                                      <p>5. En caso de que el alumno desee realizar un cambio de horario de su diagnóstico, deberá comunicarse con la organización con un mínimo de  08 horas de anticipación y solicitar dicho cambio , este cambio podrá ser posible, en caso de existir disponibilidad en la agenda de citas, de lo contrario el participante deberá aplicar el estatus de cita exclusiva.</p>
+                              <p>5. En caso de que el alumno desee realizar un cambio de horario de su diagnóstico, deberá comunicarse con la organización con un mínimo de 08 horas de anticipación y solicitar dicho cambio, este cambio podrá ser posible en caso de existir disponibilidad en la agenda de citas, de lo contrario el participante deberá aplicar el estatus de cita exclusiva.</p>
 
-                                      <div class="f-18 f-700"> Tipos de citas  </div><br>
+                              <div class="f-18 f-700"> Tipos de citas  </div><br>
 
-                                      <p>✔  Estatus de cita exclusiva: 10.000 $ <br>
-                                        ✔ Cita retraso: 4.000$ <br>
-                                      </p>
+                              <p>✔  Estatus de cita exclusiva: 10.000 $ <br>
+                                ✔ Cita retraso: 4.000$ <br>
+                              </p>
 
               
 
