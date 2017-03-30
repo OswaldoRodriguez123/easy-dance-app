bb5291af7da7ae5372926fb0e51c74e3da17e150
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-03-30 15:05:56 -0400

    Cambios del jueves

--- resources/views/configuracion/academia/planilla.blade.php
+++ resources/views/configuracion/academia/planilla.blade.php
@@ -1227,7 +1227,7 @@
     $(document).ready(function(){
 
       console.log("{{$academia->programacion}}")
-      if("{{$academia->programacion}}" != undefined){
+      if("{{$academia->programacion}}" || "{{$academia->programacion}}" != 'undefined'){
         $('#fileinput').removeClass('fileinput-new')
         $("input[name=programacion]").attr('name','')
         $('#programacion').attr('name','programacion')
