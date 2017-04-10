b713d6e483a6b0c0d48e427e4f913fcdfe3382eb
Cesar Ramirez <cesar231191@gmail.com>
2017-04-10 18:08:30 -0430

    Cambios del lunes

--- resources/views/agendar/clase_grupal/historial_general.blade.php
+++ resources/views/agendar/clase_grupal/historial_general.blade.php
@@ -36,7 +36,7 @@
                     <div class="block-header">
                         <!-- <a class="btn-blanco m-r-10 f-16" href="{{url('/')}}/asistencia" onclick="procesando()"> <i class="zmdi zmdi-chevron-left zmdi-hc-fw"></i> Sección de Asistencias</a> -->
 
-                        <a class="btn-blanco m-r-10 f-16" href="{{url('/')}}/agendar/clases-grupales" onclick="procesando()"> <i class="zmdi zmdi-chevron-left zmdi-hc-fw"></i> Sección de Clases Grupales</a>
+                        <a class="btn-blanco m-r-10 f-16" href="{{url('/')}}/agendar/clases-grupales/riesgo-ausencia" onclick="procesando()"> <i class="zmdi zmdi-chevron-left zmdi-hc-fw"></i> Sección de Clases Grupales</a>
 
                         <ul class="tab-nav tab-menu" role="tablist" data-menu-color="azul" style="float: right; margin-top: -10px; width: 40%;">
                             <li><a href="#modalParticipantes" class="azul" data-toggle="modal" style="padding:0 5px 0 0;"><div class="icon_a icon_a-participantes f-30 text-center" style="color:#2196f3;"></div><p style=" font-size: 10px; color:#2196f3;">Participantes</p></a></li>
