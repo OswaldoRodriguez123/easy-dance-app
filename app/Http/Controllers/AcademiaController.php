519a91f4fdf4e7a1c96de359f725eeda4e6cd6b8
Cesar Ramirez <cesar231191@gmail.com>
2017-04-12 16:51:09 -0430

    Cambios del miercoles

--- app/Http/Controllers/AcademiaController.php
+++ app/Http/Controllers/AcademiaController.php
@@ -103,35 +103,35 @@ class AcademiaController extends BaseController {
         // 	$patrocinador->save();
         // }
 
-        $facturas = ItemsFactura::all();
+        // $facturas = ItemsFactura::all();
 
-        foreach($facturas as $factura){
+        // foreach($facturas as $factura){
 
-            if($factura->tipo == 9){
-                $inscripcion_clase_personalizada = InscripcionClasePersonalizada::find($factura->item_id);
-                if($inscripcion_clase_personalizada){
+        //     if($factura->tipo == 9){
+        //         $inscripcion_clase_personalizada = InscripcionClasePersonalizada::find($factura->item_id);
+        //         if($inscripcion_clase_personalizada){
 
-                    $clase_personalizada = ClasePersonalizada::withTrashed()->find($inscripcion_clase_personalizada->clase_personalizada_id);
-                    $factura->nombre = $clase_personalizada->nombre;
-                    $factura->save();
-                }
-            }
-        }
+        //             $clase_personalizada = ClasePersonalizada::withTrashed()->find($inscripcion_clase_personalizada->clase_personalizada_id);
+        //             $factura->nombre = $clase_personalizada->nombre;
+        //             $factura->save();
+        //         }
+        //     }
+        // }
 
-        $facturas = ItemsFacturaProforma::all();
+        // $facturas = ItemsFacturaProforma::all();
 
-        foreach($facturas as $factura){
+        // foreach($facturas as $factura){
 
-            if($factura->tipo == 9){
-                $inscripcion_clase_personalizada = InscripcionClasePersonalizada::find($factura->item_id);
-                if($inscripcion_clase_personalizada){
+        //     if($factura->tipo == 9){
+        //         $inscripcion_clase_personalizada = InscripcionClasePersonalizada::find($factura->item_id);
+        //         if($inscripcion_clase_personalizada){
 
-                    $clase_personalizada = ClasePersonalizada::withTrashed()->find($inscripcion_clase_personalizada->clase_personalizada_id);
-                    $factura->nombre = $clase_personalizada->nombre;
-                    $factura->save();
-                }
-            }
-        }
+        //             $clase_personalizada = ClasePersonalizada::withTrashed()->find($inscripcion_clase_personalizada->clase_personalizada_id);
+        //             $factura->nombre = $clase_personalizada->nombre;
+        //             $factura->save();
+        //         }
+        //     }
+        // }
 
         // $facturas = ItemsFacturaProforma::all();
 
