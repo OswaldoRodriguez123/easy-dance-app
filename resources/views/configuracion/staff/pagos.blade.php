dd4f8c2e88c8be7733cf5dc0f33c072cf2303029
Oswaldo Rodriguez <bfsraptor@gmail.com>
2017-06-01 17:14:43 -0400

    Cambios del jueves

--- resources/views/configuracion/staff/pagos.blade.php
+++ resources/views/configuracion/staff/pagos.blade.php
@@ -365,10 +365,11 @@
                 var nTitle="Ups! ";
                 var nMensaje=respuesta.mensaje;
 
-                $.each(respuesta.array, function (i, id) {
-                  $.each(por_pagar, function (index, array) {
+                $.each(respuesta.array, function (id) {
+                  $.each(por_pagar, function (array) {
+                    console.log(array);
                     if(array.id == id){
-                        por_pagar.splice( $.inArray(por_pagar[index], por_pagar), 1 );
+                        por_pagar.splice( $.inArray(array, por_pagar), 1 );
 
                         t.row($('#'+id))
                           .remove()
