70063a0cc650e3c17978718761dae23cd40b90e0
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-06-06 10:43:31 -0400

    Cambios del martes

--- resources/views/reportes/administrativo.blade.php
+++ resources/views/reportes/administrativo.blade.php
@@ -406,6 +406,41 @@
                         }
                     }
         });
+
+        h=$('#table_egresos').DataTable({
+                processing: true,
+                serverSide: false,
+                pageLength: 25, 
+                bPaginate: false,
+                bInfo: false,
+                bFilter:false, 
+                bSort:false, 
+                language: {
+                    processing:     "Procesando ...",
+                    search:         '<div class="input-group"><span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>',
+                    searchPlaceholder: "BUSCAR",
+                    lengthMenu:     " ",
+                    info:           "Mostrando _START_ a _END_ de _TOTAL_ Registros",
+                    infoEmpty:      "Mostrando 0 a 0 de 0 Registros",
+                    infoFiltered:   "(filtrada de _MAX_ registros en total)",
+                    infoPostFix:    "",
+                    loadingRecords: "...",
+                    zeroRecords:    "No se encontraron registros coincidentes",
+                    emptyTable:     "No hay datos disponibles en la tabla",
+                    paginate: {
+                        first:      "Primero",
+                        previous:   "Anterior",
+                        next:       "Siguiente",
+                        last:       "Ultimo"
+                    },
+                    aria: {
+                        sortAscending:  ": habilitado para ordenar la columna en orden ascendente",
+                        sortDescending: ": habilitado para ordenar la columna en orden descendente"
+                    }
+                }
+
+
+            });
     });
 
         $("#guardar").click(function(){
