2a17696f76da290fccb8a38a290217114da94646
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-06-28 19:15:59 -0400

    Cambios del miercoles

--- app/Http/Controllers/ClaseGrupalController.php
+++ app/Http/Controllers/ClaseGrupalController.php
@@ -694,12 +694,15 @@ class ClaseGrupalController extends BaseController {
                             $index_inicial = $index;
                         }
                     }
-                    
                     // $index_inicial = array_search($dia_inscripcion, $array_dias_clases_inscripcion);
 
+                    if($alumno->id == 521){
+                        dd($index_inicial);
+                    }
+
                     $fecha_ultima_asistencia = $fecha_a_comparar->toDateString();
-                    // $array_fecha_a_comparar = array();
-                    // $array_dias_tmp = array();
+                    $array_fecha_a_comparar = array();
+                    $array_dias_tmp = array();
 
                     while($fecha_a_comparar < $fecha_de_finalizacion){
                         for($k = $index_inicial; $k < count($array_dias); $k++){
