a73a2fbb17560b33c6aaebf236d700aa8a56f22f
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-08-26 15:59:00 -0400

    Cambios del sabado

--- resources/views/login/seleccionar-tipo.blade.php
+++ resources/views/login/seleccionar-tipo.blade.php
@@ -114,7 +114,7 @@
         type: 'POST',
         dataType: 'json',
         success:function(respuesta){
-          window.open(route, '_blank');_principal; 
+          window.location = route_principal; 
         },
         error:function(msj){
           swal('Solicitud no procesada','Error','error');
