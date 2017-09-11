cfa327daed363c60aa096b007767d79c7c4f5109
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-09-11 18:28:44 -0400

    Cambios del lunes

--- resources/views/asistencia/generar.blade.php
+++ resources/views/asistencia/generar.blade.php
@@ -965,9 +965,9 @@
           },
           error:function (msj, ajaxOptions, thrownError){
             setTimeout(function(){ 
-              if (typeof msj.responseJSON === "undefined") {
-                window.location = "{{url('/')}}/error";
-              }
+              // if (typeof msj.responseJSON === "undefined") {
+              //   window.location = "{{url('/')}}/error";
+              // }
               var nType = 'danger';
               if(msj.responseJSON.status=="ERROR"){
                 errores(msj.responseJSON.errores);
