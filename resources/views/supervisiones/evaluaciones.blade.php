e49406e998a588f3461feb71f7dacab2ac6b8b84
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-09-22 14:04:48 -0400

    Cambios del viernes

--- resources/views/supervisiones/evaluaciones.blade.php
+++ resources/views/supervisiones/evaluaciones.blade.php
@@ -184,7 +184,7 @@
         order: [[0, 'desc']],
         fnRowCallback: function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
           $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4),td:eq(5)', nRow).addClass( "text-center" );
-          $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4)', nRow).attr( "onclick","previa(this)" );
+          $('td:eq(0),td:eq(1),td:eq(2),td:eq(3),td:eq(4),td:eq(5)', nRow).attr( "onclick","previa(this)" );
         },
         language: {
                         processing:     "Procesando ...",
