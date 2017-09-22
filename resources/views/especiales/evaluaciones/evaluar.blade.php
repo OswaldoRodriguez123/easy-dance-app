cc928ae305158b495d6e41223d53245949d381e2
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-09-22 14:27:33 -0400

    Cambios del viernes

--- resources/views/especiales/evaluaciones/evaluar.blade.php
+++ resources/views/especiales/evaluaciones/evaluar.blade.php
@@ -173,15 +173,17 @@
                   <label for="observacion" id="id-observacion">Observaciones</label> <i class="p-l-5 tm-icon zmdi zmdi-help ayuda mousedefault" data-trigger="hover" data-toggle="popover" data-placement="right" data-content="ingresa las observaciones y detalles correspondientes a la evaluación del alumno" title="" data-original-title="Ayuda"></i>
                   <br></br>
 
-                  <div class="fg-line">
-                    <textarea class="form-control" id="observacion" name="observacion" rows="2" placeholder="1000 Caracteres">{{$evaluacion->observacion}}</textarea>
-                  </div>
+                  <textarea class="form-control caja" style="height:100%" id="observacion" name="observacion" rows="8" placeholder="2000 Caracteres" maxlength="2000" onkeyup="countChar(this)">{{$evaluacion->observacion}}</textarea>
+                  <div class="opaco-0-8 text-right">Resta <span id="charNum">2000</span> Caracteres</div>
+
                   <div class="has-error" id="error-observacion">
                     <span >
                       <small class="help-block error-span" id="error-observacion_mensaje" ></small> 
                     </span>
                   </div>
                 </div>
+
+                <div class="clearfix p-b-35"></div>
 
     
              <!--  <div class="col-sm-12">
@@ -697,6 +699,14 @@
           $("#error-"+campo[i]+"_mensaje").html('');
       }
     }
+    function countChar(val) {
+      var len = val.value.length;
+      if (len >= 2000) {
+        val.value = val.value.substring(0, 2000);
+      } else {
+        $('#charNum').text(2000 - len);
+      }
+    };
 
   </script>
 
