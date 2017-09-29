d8f5628ac118a7dc32dc0720f210f500070df309
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-09-29 14:27:15 -0400

    Cambios del viernes

--- resources/views/reportes/inactivos_congelados.blade.php
+++ resources/views/reportes/inactivos_congelados.blade.php
@@ -283,7 +283,7 @@
             var token = $('input:hidden[name=_token]').val();
             var datos = $( "#formFiltro" ).serialize();
 
-            // procesando();
+            procesando();
 
             t.clear().draw();
 
