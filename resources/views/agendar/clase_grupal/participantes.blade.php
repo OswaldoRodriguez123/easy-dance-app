5b24de744919c6968839750b3b788e05c69d4b97
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-11-13 10:31:53 -0400

    Cambios del lunes

--- resources/views/agendar/clase_grupal/participantes.blade.php
+++ resources/views/agendar/clase_grupal/participantes.blade.php
@@ -1235,6 +1235,8 @@
                                 	'Cantidad que adeuda: ' . number_format($deuda, 2, '.' , '.')  . '<br>'.
                                   'Modalidad de pago: <span id="tipo_pago_'.$id.'">' . $tipo_pago . '</span><br>'.
                                   'Registro de llamada: ' . $alumno['llamadas'] . '<br>'.
+                                  'Celular: ' . $alumno['celular'] . '<br>'.
+                                  'Teléfono: ' . $alumno['telefono'] . '<br>'.
                                   // 'Inasistencias: ' . $alumno['inasistencias'] . '<br>'.
                                   // 'Ultima Asistencia: ' . $alumno['ultima_asistencia'] . '<br>'.
                                 '</p>';
--- resources/views/vista_instructor/clase_grupal.blade.php
+++ resources/views/vista_instructor/clase_grupal.blade.php
@@ -104,7 +104,7 @@
                                     <th class="text-center" data-column-id="nombre" data-order="desc">Nombre</th>
                                     <th class="text-center" data-column-id="especialidad" data-order="desc">Especialidad</th>
                                     <th class="text-center" data-column-id="hora" data-order="desc">Hora [Inicio - Final]</th>
-                                    <th class="text-center operacion" data-column-id="operacion" data-order="desc">Operaciones</th>
+<!--                                     <th class="text-center operacion" data-column-id="operacion" data-order="desc">Operaciones</th> -->
                                 </tr>
                             </thead>
                             <tbody class="text-center" >
@@ -359,13 +359,13 @@
                 operacion += 'Ver Progreso'
                 operacion += '</a></li>'
                 operacion += '</ul></div></li></ul>'
-   
+                
                 var rowNode=t.row.add( [
                 ''+inicio+'',
                 ''+array.clase_grupal_nombre+'',
                 ''+array.especialidad_nombre+'',
                 ''+array.hora_inicio+ ' '+array.hora_final+'',
-                ''+operacion+'',
+                // ''+operacion+'',
                 ] ).draw(false).node();
                 $( rowNode )
                     .attr('id',array.id)
